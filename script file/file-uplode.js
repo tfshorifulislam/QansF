edb3d1bb95ea00b19c481028ab00d46ee39679d3
tfshorifulislam <tfshorifulislam@gmail.com>
2026-02-21 05:14:35 +0600

    file upload button issue fixed

--- script file/file-uplode.js
+++ script file/file-uplode.js
@@ -1,8 +1,9 @@
+// upload button for large device
 document.getElementById("uploadBtn").addEventListener("click", function() {
     document.getElementById("fileInput").click();
 });
 
-
+// upload button for mobile
 document.getElementById("uploadBtn2").addEventListener("click", function() {
     document.getElementById("fileInput2").click();
 });
