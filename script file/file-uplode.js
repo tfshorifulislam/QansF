d69f09a63df86cc155e12b33faedded873f22d3d
tfshorifulislam <tfshorifulislam@gmail.com>
2026-02-21 05:11:23 +0600

    file uploded system add

---
+++ script file/file-uplode.js
@@ -0,0 +1,8 @@
+document.getElementById("uploadBtn").addEventListener("click", function() {
+    document.getElementById("fileInput").click();
+});
+
+
+document.getElementById("uploadBtn2").addEventListener("click", function() {
+    document.getElementById("fileInput2").click();
+});
